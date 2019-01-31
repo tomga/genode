76bb9b75b4690bcce90a0a70beeec45e2d55d23d
Tomasz Gajewski <tomga@wp.pl>
2019-01-31 22:24:15 +0100

    support for rpi3bplus

---
+++ repos/base-hw/src/bootstrap/spec/rpi3bplus/board.h
@@ -0,0 +1,37 @@
+/*
+ * \brief   Rpi3bplus specific board definitions
+ * \author  Tomasz Gajewski
+ * \date    2019-01-30
+ */
+
+/*
+ * Copyright (C) 2017 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+#ifndef _SRC__BOOTSTRAP__SPEC__RPI3BPLUS__BOARD_H_
+#define _SRC__BOOTSTRAP__SPEC__RPI3BPLUS__BOARD_H_
+
+#include <drivers/defs/rpi3bplus.h>
+#include <drivers/uart/mini.h>
+
+#include <hw/spec/arm/cortex_a15.h>
+#include <hw/spec/arm/lpae.h>
+#include <spec/arm/cpu.h>
+#include <spec/arm/pic.h>
+
+namespace Board {
+
+	using namespace Rpi3bplus;
+	using Cpu_mmio = Hw::Cortex_a15_mmio<IRQ_CONTROLLER_BASE>;
+	using Serial   = Genode::Mini_uart;
+
+	enum {
+		UART_BASE  = Rpi3bplus::PL011_0_MMIO_BASE,
+		UART_CLOCK = Rpi3bplus::PL011_0_CLOCK,
+	};
+}
+
+#endif /* _SRC__BOOTSTRAP__SPEC__RPI3BPLUS__BOARD_H_ */
---
+++ repos/base-hw/src/core/spec/rpi3bplus/platform_services.cc
@@ -0,0 +1,43 @@
+/*
+ * \brief   Platform specific services for Rpi3bplus
+ * \author  Tomasz Gajewski
+ * \date    2019-01-30
+ */
+
+/*
+ * Copyright (C) 2014-2017 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+/* Genode includes */
+#include <base/service.h>
+#include <base/heap.h>
+
+/* core includes */
+#include <platform.h>
+#include <platform_services.h>
+#include <core_service.h>
+#include <map_local.h>
+#include <vm_root.h>
+#include <platform.h>
+
+extern Genode::addr_t hypervisor_exception_vector;
+
+/*
+ * Add ARM virtualization specific vm service
+ */
+void Genode::platform_add_local_services(Rpc_entrypoint *ep,
+                                         Sliced_heap *sh,
+                                         Registry<Service> *services)
+{
+	using namespace Genode;
+
+	map_local(Platform::core_phys_addr((addr_t)&hypervisor_exception_vector),
+	          Hw::Mm::hypervisor_exception_vector().base, 1,
+	          Hw::PAGE_FLAGS_KERN_TEXT);
+
+	static Vm_root vm_root(ep, sh);
+	static Core_service<Vm_session_component> vm_service(*services, vm_root);
+}
