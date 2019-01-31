f20de255592889ea264037a818ba2c6bc6fa2a64
Tomasz Gajewski <tomga@wp.pl>
2019-01-31 22:24:15 +0100

    support for rpi3bplus

---
+++ repos/base-hw/src/core/spec/rpi3bplus/timer.cc
@@ -0,0 +1,62 @@
+/*
+ * \brief   Timer implementation specific to Rpi
+ * \author  Norman Feske
+ * \author  Stefan Kalkowski
+ * \author  Martin Stein
+ * \date    2016-01-07
+ */
+
+/*
+ * Copyright (C) 2016-2017 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+/* core includes */
+#include <platform.h>
+#include <kernel/timer.h>
+
+using namespace Genode;
+using namespace Kernel;
+
+
+Timer_driver::Timer_driver(unsigned cpu_id)
+:
+  Mmio(Platform::mmio_to_virt(Board::SYSTEM_TIMER_MMIO_BASE)),
+  cpu_id(cpu_id)
+{
+	Genode::log("Timer_driver - ", cpu_id);
+}
+
+
+void Timer::_start_one_shot(time_t const ticks)
+{
+	_driver.write<Driver::Cs::M1>(1);
+	_driver.read<Driver::Cs>();
+	_driver.write<Driver::Clo>(0);
+	_driver.write<Driver::Cmp>(_driver.read<Driver::Clo>() + ticks);
+}
+
+
+time_t Timer::_ticks_to_us(time_t const ticks) const {
+	return ticks / Driver::TICS_PER_US; }
+
+
+time_t Timer::us_to_ticks(time_t const us) const {
+	return us * Driver::TICS_PER_US; }
+
+
+time_t Timer::_max_value() const {
+	return (Driver::Clo::access_t)~0; }
+
+
+time_t Timer::_value()
+{
+	Driver::Cmp::access_t const cmp = _driver.read<Driver::Cmp>();
+	Driver::Clo::access_t const clo = _driver.read<Driver::Clo>();
+	return cmp > clo ? cmp - clo : 0;
+}
+
+
+unsigned Timer::interrupt_id() const { return Board::SYSTEM_TIMER_IRQ; }
