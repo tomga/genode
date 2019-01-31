080588856ce3d6b7dfb9596645fee8a05ab5f72c
Tomasz Gajewski <tomga@wp.pl>
2019-01-31 22:24:15 +0100

    support for rpi3bplus

---
+++ repos/base-hw/src/core/spec/rpi3bplus/board.h
@@ -0,0 +1,36 @@
+/*
+ * \brief  Board driver for core
+ * \author Stefan Kalkowski
+ * \date   2017-04-27
+ */
+
+/*
+ * Copyright (C) 2017 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+#ifndef _CORE__SPEC__RPI3BPLUS__BOARD_H_
+#define _CORE__SPEC__RPI3BPLUS__BOARD_H_
+
+/* base includes */
+#include <drivers/defs/rpi3bplus.h>
+#include <drivers/uart/mini.h>
+
+#include <hw/spec/arm/cortex_a15.h>
+
+namespace Board {
+	using namespace Rpi3bplus;
+	using Cpu_mmio = Hw::Cortex_a15_mmio<IRQ_CONTROLLER_BASE>;
+	using Serial   = Genode::Mini_uart;
+
+	enum {
+		UART_BASE  = Rpi3bplus::PL011_0_MMIO_BASE,
+		UART_CLOCK = Rpi3bplus::PL011_0_CLOCK,
+	};
+
+	static constexpr bool SMP = true;
+}
+
+#endif /* _CORE__SPEC__RPI3BPLUS__BOARD_H_ */
---
+++ repos/base/include/drivers/defs/rpi3bplus.h
@@ -0,0 +1,97 @@
+/*
+ * \brief  MMIO and IRQ definitions for the Raspberry Pi
+ * \author Tomasz Gajewski
+ * \date   2019-01-30
+ */
+
+/*
+ * Copyright (C) 2013-2017 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+#ifndef _INCLUDE__DRIVERS__DEFS__RPI_H_
+#define _INCLUDE__DRIVERS__DEFS__RPI_H_
+
+/* Genode includes */
+#include <util/mmio.h>
+
+namespace Rpi3bplus {
+	enum {
+		RAM_0_BASE = 0x00000000,
+		RAM_0_SIZE = 0x10000000, /* XXX ? */
+
+		MMIO_0_BASE = 0x3f000000,
+		MMIO_0_SIZE = 0x02000000,
+
+		/*
+		 * IRQ numbers   0..7 refer to the basic IRQs.
+		 * IRQ numbers  8..39 refer to GPU IRQs  0..31.
+		 * IRQ numbers 40..71 refer to GPU IRQs 32..63.
+		 */
+		GPU_IRQ_BASE = 8,
+
+		SYSTEM_TIMER_IRQ       = GPU_IRQ_BASE + 1,
+		SYSTEM_TIMER_MMIO_BASE = 0x3f003000,
+		SYSTEM_TIMER_MMIO_SIZE = 0x1000,
+		SYSTEM_TIMER_CLOCK     = 1200000,
+
+		/* PL011_0_IRQ       = 57, */
+		/* PL011_0_MMIO_BASE = 0x3f201000, */
+		PL011_0_IRQ       = 166,
+		PL011_0_MMIO_BASE = 0x3f215000,
+		PL011_0_MMIO_SIZE = 0x1000,
+		PL011_0_CLOCK     = 250000000,
+                /* PL011_0_CLOCK     = 3000000, */
+
+		IRQ_CONTROLLER_BASE = 0x3F00b200,
+		IRQ_CONTROLLER_SIZE = 0x100,
+
+                // FROM EXYNOS FOR NOW
+		IRQ_CONTROLLER_VT_CTRL_BASE = 0x10484000,
+		IRQ_CONTROLLER_VT_CTRL_SIZE = 0x1000,
+		IRQ_CONTROLLER_VT_CPU_BASE  = 0x10486000,
+		IRQ_CONTROLLER_VT_CPU_SIZE  = 0x1000,
+
+                // FROM EXYNOS FOR NOW
+		/* virtual interrupts */
+		VT_MAINTAINANCE_IRQ = 25,
+		VT_TIMER_IRQ        = 27,
+
+		GPIO_CONTROLLER_BASE = 0x3f200000,
+		GPIO_CONTROLLER_SIZE = 0x1000,
+
+		USB_DWC_OTG_BASE = 0x3f980000,
+		USB_DWC_OTG_SIZE = 0x20000,
+
+		/* timer */
+		TIMER_IRQ = 0,
+
+		/* USB host controller */
+		DWC_IRQ = 17,
+
+		/* CPU cache */
+		CACHE_LINE_SIZE_LOG2 = 6,
+
+                // FROM EXYNOS FOR NOW
+		/* hardware name of the primary processor */
+		PRIMARY_MPIDR_AFF_0 = 0,
+
+                // FROM EXYNOS FOR NOW
+		/* IRAM */
+		IRAM_BASE = 0x02020000,
+
+		/* SD card */
+		SDHCI_BASE = MMIO_0_BASE + 0x300000,
+		SDHCI_SIZE = 0x100,
+		SDHCI_IRQ  = 62,
+	};
+
+	enum Videocore_cache_policy { NON_COHERENT = 0,
+	                              COHERENT     = 1,
+	                              L2_ONLY      = 2,
+	                              UNCACHED     = 3 };
+};
+
+#endif /* _INCLUDE__DRIVERS__DEFS__RPI_H_ */
