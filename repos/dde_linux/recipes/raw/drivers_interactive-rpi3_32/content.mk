934cb13e512d88041bbeeccb23071bd261638356
Tomasz Gajewski <tomga@wp.pl>
2019-02-27 23:31:44 +0100

    framebuffer: support for rpi2 and rpi3_32
    Working 1024x768 buffer
    * fixed size of property message's buf_size
    * added conversion of received framebuffer's address from bus address to
      physical address
    * IMPORTANT: explicitly asked for uncached dataspace buffer
    * added proper response code checks
    * added platform_drv.config to inform driver about rpi version

--- repos/dde_linux/recipes/raw/drivers_interactive-rpi3_32/content.mk
+++ repos/dde_linux/recipes/raw/drivers_interactive-rpi3_32/content.mk
@@ -1,4 +1,4 @@
 content: drivers.config fb_drv.config platform_drv.config
 
 drivers.config fb_drv.config platform_drv.config:
-	cp $(REP_DIR)/recipes/raw/drivers_interactive-rpi/$@ $@
+	cp $(REP_DIR)/recipes/raw/drivers_interactive-rpi3_32/$@ $@
