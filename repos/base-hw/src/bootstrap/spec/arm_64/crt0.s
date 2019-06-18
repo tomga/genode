97db784af04d6872fd5747be0947ce60d997eb1c
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2019-06-18 16:31:03 +0200

    fixup "hw: support for ARM64 Raspberry Pi 3"

--- repos/base-hw/src/bootstrap/spec/arm_64/crt0.s
+++ repos/base-hw/src/bootstrap/spec/arm_64/crt0.s
@@ -16,70 +16,50 @@
 	.global _start
 	_start:
 
-	/****************
-	 ** Enable FPU **
-	 ****************/
-
-	mov x0, #0b11
-	lsl x0, x0, #20
-	msr cpacr_el1, x0
-
-
 	/***********************
 	 ** Detect CPU number **
 	 ***********************/
 
 	mrs x0, mpidr_el1
 	and x0, x0, #0b11111111
-	cmp x0, #0
-	bne __endless_sleep
+	cbz x0, _crt0_fill_bss_zero
+	wfe
+	b _start
 
 
 	/***************************
 	 ** Zero-fill BSS segment **
 	 ***************************/
 
-	ldr x0, =_bss_local_start
-	ldr x1, =_bss_local_end
+	_crt0_fill_bss_zero:
+	ldr x0, =_bss_start
+	ldr x1, =_bss_end
 	1:
 	cmp x1, x0
-	ble 2f
-	str xzr, [x0]
-	add x0, x0, #8
-	b   1b
-	2:
+	b.eq _crt0_enable_fpu
+	str xzr, [x0], #8
+	b 1b
 
 
-	/************************************
-	 ** Jump to high-level entry point **
-	 ************************************/
+	/****************
+	 ** Enable FPU **
+	 ****************/
 
-	ldr x0, =_start_stack        /* load stack address  into x0 */
-	msr vbar_el1, x0
+	_crt0_enable_fpu:
+	mov x0, #0b11
+	lsl x0, x0, #20
+	msr cpacr_el1, x0
+
+
+	/**********************
+	 ** Initialize stack **
+	 **********************/
+
+	ldr x0, =_crt0_start_stack
 	mov sp, x0
+	1: b 1b
 	bl init
-
-	__endless_sleep:
-		wfe
-		1: b 1b
-
-	_bss_local_start:
-	.quad _bss_start
-
-	_bss_local_end:
-	.quad _bss_end
 
 	.p2align 4
 	.space 0x4000
-	_start_stack:
-
-	.rept 512
-	nop
-	.endr
-	mrs x24, far_el1
-	mrs x25, ttbr0_el1
-	mrs x26, ttbr1_el1
-	mrs x27, elr_el1
-	mrs x28, esr_el1
-	1: b 1b
-
+	_crt0_start_stack:
