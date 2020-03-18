7d1f158d6836ed8e52e0feecc46aa455b1ae1841
Christian Helmuth <christian.helmuth@genode-labs.com>
2020-03-18 16:27:28 +0100

    Update OpenSSL to version 1.1.1i
    Note, OpenSSL now comes as one combined depot archive *openssl* that
    replaces the former *libssl* and *libcrypto* archives. The libraries are
    still separate binaries for compatibility with legacy software.
    
    Issue #3773

--- repos/libports/lib/import/import-libssl.mk
+++ repos/libports/lib/import/import-libssl.mk
@@ -1,1 +1,6 @@
+LIB_OPENSSL_DIR = $(call select_from_repositories,src/lib/openssl)
+
+ARCH = $(filter 32bit 64bit,$(SPECS))
+
 INC_DIR += $(call select_from_ports,openssl)/include
+INC_DIR += $(LIB_OPENSSL_DIR)/spec/$(ARCH)
