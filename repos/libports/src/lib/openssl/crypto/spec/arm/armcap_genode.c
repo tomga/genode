61a530a485241b88a049efadd0de42f82647c918
Christian Helmuth <christian.helmuth@genode-labs.com>
2021-02-18 14:55:31 +0100

    squash "openssl: do not depend on ARM CPU probing" (COMMIT MESSAGE)
    openssl: do not depend on ARM CPU probing
    
    With this commit libcrypto does not use ARM NEON extension as long as
    SPECS includes "neon". arm_v7a does declare "neon" per default while
    arm_v8a does.

--- repos/libports/src/lib/openssl/crypto/spec/arm/armcap_genode.c
+++ repos/libports/src/lib/openssl/crypto/spec/arm/armcap_genode.c
@@ -1,11 +1,11 @@
 /*
- * \brief  Component environment
- * \author Norman Feske
- * \date   2015-12-17
+ * \brief  OpenSSL ARM CPU capabilities (no NEON)
+ * \author Pirmin Duss
+ * \date   2021-02-17
  */
 
 /*
- * Copyright (C) 2015-2017 Genode Labs GmbH
+ * Copyright (C) 2021 Genode Labs GmbH
  *
  * This file is part of the Genode OS framework, which is distributed
  * under the terms of the GNU Affero General Public License version 3.
@@ -17,13 +17,6 @@
 
 unsigned int OPENSSL_armcap_P = 0;
 
+void OPENSSL_cpuid_setup(void) { }
 
-void OPENSSL_cpuid_setup(void)
-{
-}
-
-
-uint32_t OPENSSL_rdtsc(void)
-{
-	return 0;
-}
+uint32_t OPENSSL_rdtsc(void) { return 0; }
--- repos/libports/src/lib/openssl/crypto/spec/neon/armcap_genode.c
+++ repos/libports/src/lib/openssl/crypto/spec/neon/armcap_genode.c
@@ -1,11 +1,11 @@
 /*
- * \brief  Component environment
- * \author Norman Feske
- * \date   2015-12-17
+ * \brief  OpenSSL ARM CPU capabilities (with NEON support)
+ * \author Pirmin Duss
+ * \date   2021-02-17
  */
 
 /*
- * Copyright (C) 2015-2017 Genode Labs GmbH
+ * Copyright (C) 2021 Genode Labs GmbH
  *
  * This file is part of the Genode OS framework, which is distributed
  * under the terms of the GNU Affero General Public License version 3.
@@ -18,13 +18,6 @@
 
 unsigned int OPENSSL_armcap_P = ARMV7_NEON;
 
+void OPENSSL_cpuid_setup(void) { }
 
-void OPENSSL_cpuid_setup(void)
-{
-}
-
-
-uint32_t OPENSSL_rdtsc(void)
-{
-	return 0;
-}
+uint32_t OPENSSL_rdtsc(void) { return 0; }
