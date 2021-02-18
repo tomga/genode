2561dc0a3619f21304ef9af799e765ca49245192
Christian Helmuth <christian.helmuth@genode-labs.com>
2021-02-18 14:26:43 +0100

    fixup "Update OpenSSL to version 1.1.1i"
    Issue #3773

--- repos/libports/lib/import/import-libssl.mk
+++ repos/libports/lib/import/import-libssl.mk
@@ -1,5 +1,7 @@
 LIB_OPENSSL_DIR = $(call select_from_repositories,src/lib/openssl)
 
+LIBS += libcrypto
+
 ARCH = $(filter 32bit 64bit,$(SPECS))
 
 INC_DIR += $(call select_from_ports,openssl)/include
